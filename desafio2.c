ab17bc6e767b3f1e4e4f121ca458eadff7c53af4
Victor Hugo <163581915+victrhugo@users.noreply.github.com>
2024-10-24 13:29:19 -0300

    Create desafio2.c

---
+++ desafio2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int pertenceFibonacci(int n) {
+    int a = 0, b = 1, temp;
+
+    if (n == a || n == b) {
+        return 1;
+    }
+
+    while (b < n) {
+        temp = b;
+        b = a + b; 
+        a = temp;
+
+        if (b == n) {
+            return 1; 
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    int numero;
+
+    printf("Informe um número: ");
+    scanf("%d", &numero);
+
+    if (pertenceFibonacci(numero)) {
+        printf("O número %d pertence à sequência de Fibonacci.\n", numero);
+    } else {
+        printf("O número %d não pertence à sequência de Fibonacci.\n", numero);
+    }
+
+    return 0;
+}
